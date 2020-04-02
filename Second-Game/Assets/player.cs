8b7f8ff030305c1da5fbb8e3a8c2032dcba60770
Kowsihan <kowsihan2sk@gmail.com>
2020-04-02 23:28:48 +0530

    created a moving player(box)

---
+++ Second-Game/Assets/player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    float speed = 10;
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 direction = input.normalized;
+        Vector3 velocity = direction * speed;
+        Vector3 moveAmount = velocity * Time.deltaTime;
+
+        transform.Translate(moveAmount);
+    }
+}
