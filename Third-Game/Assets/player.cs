58bbac9639e86e4943555dcdc7215d3048198efb
Kowsihan <kowsihan2sk@gmail.com>
2020-04-04 02:22:28 +0530

    Coin-Game added
    -player(cube) move to collect coin while avoiding rigid blocks

---
+++ Third-Game/Assets/player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float speed = 6;
+    int coinCount;
+    Vector3 velocity;
+    Rigidbody myRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 direction = input.normalized;
+        velocity = direction * speed;
+    }
+
+    private void FixedUpdate()
+    {
+        myRigidbody.position += velocity * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider triggerCollider)
+    {
+        if(triggerCollider.tag == "Coin")
+        {
+            Destroy(triggerCollider.gameObject);
+            coinCount++;
+            print(coinCount);
+        }
+    }
+}
