4ef04c752af11b49e48661199c6bd7ef2beee430
Kowsihan <kowsihan2sk@gmail.com>
2020-04-04 23:26:55 +0530

    Falling Blocks Game

---
+++ Falling Blocks Game/Assets/Scripts/FallingBlock.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingBlock : MonoBehaviour
+{
+    public Vector2 speedMinMax;
+    float speed;
+
+    float visibleHeightthreshold;
+
+     void Start()
+    {
+        speed = Mathf.Lerp(speedMinMax.x, speedMinMax.y, Difficulty.GetDifficultyPercent());
+
+        visibleHeightthreshold = -Camera.main.orthographicSize - transform.localScale.y;
+    }
+    void Update()
+    {
+        transform.Translate(Vector3.down * speed * Time.deltaTime, Space.Self);
+    
+        if(transform.position.y < visibleHeightthreshold)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Falling Blocks Game/Assets/Scripts/Spawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject fallingBlockPrefab;
+    public Vector2 secondsBetweenSpawnsMinMax;
+    float nextSpawnTime;
+
+    public Vector2 spawnSizeMinMax;
+    public float spawnAngleMax;
+
+    Vector2 screenHalfSizeWorldUnits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenHalfSizeWorldUnits = new Vector2(Camera.main.aspect * Camera.main.orthographicSize, Camera.main.orthographicSize + .5f);    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextSpawnTime)
+        {
+            float secondsBetweenSpawns = Mathf.Lerp(secondsBetweenSpawnsMinMax.y, secondsBetweenSpawnsMinMax.x, Difficulty.GetDifficultyPercent());
+            nextSpawnTime = Time.time + secondsBetweenSpawns;
+
+            float spawnAngle = Random.Range(-spawnAngleMax, spawnAngleMax);
+            float spawnSize = Random.Range(spawnSizeMinMax.x, spawnSizeMinMax.y);
+            Vector2 spawnPosition = new Vector2(Random.Range(-screenHalfSizeWorldUnits.x, screenHalfSizeWorldUnits.x), screenHalfSizeWorldUnits.y + spawnSize);
+            GameObject newBlock = (GameObject)Instantiate(fallingBlockPrefab, spawnPosition, Quaternion.Euler(Vector3.forward * spawnAngle));
+            newBlock.transform.localScale = Vector2.one * spawnSize;
+        }
+    }
+}
